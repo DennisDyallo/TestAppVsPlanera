17d10583af81f5edb98647bab03a07dbcd39e8ec
Dennis Dyallo <dyallo@live.se>
2022-03-30 13:00:06 +0200

    Add project files.

---
+++ TestAppVsPlanera/Program.cs
@@ -0,0 +1,6 @@
+﻿using TestAppVsPlanera.TablaApi;
+using UtbudApi;
+
+Console.WriteLine("Hello, Olaf!");
+var tablaClient = new TablaApiClient(new HttpClient { BaseAddress = new Uri("https://planera.yoda.sr.se/tabla-api") });
+var utbudClient = new UtbudApiClient(new HttpClient { BaseAddress = new Uri("https://planera.yoda.sr.se/utbud-api") });
